f0bf7973c668ffbe5863d1619f9cb9a43e801d9e
Jeroen van der Poel <49712178+Jeroen-van-der-Poel@users.noreply.github.com>
2021-11-09 17:14:17 +0100

    input cpp

---
+++ Spick_Engine/Spick_Engine/API_Sources/Input.cpp
@@ -0,0 +1,42 @@
+#include "../API_Headers/Input.hpp"
+using namespace spic;
+
+bool Input::AnyKey() {
+
+}
+
+bool Input::AnyKeyDown() {
+
+}
+
+const Point Input::MousePosition() {
+
+}
+
+double Input::GetAxis() {
+
+}
+
+bool Input::GetKey(KeyCode key) {
+
+}
+
+bool Input::GetKeyDown(KeyCode key) {
+
+}
+
+bool Input::GetKeyUp(KeyCode key) {
+
+}
+
+bool Input::GetMouseButton(MouseButton which) {
+
+}
+
+bool Input::GetMouseButtonDown(MouseButton which) {
+
+}
+
+bool Input::GetMouseButtonUp(MouseButton which) {
+
+}
