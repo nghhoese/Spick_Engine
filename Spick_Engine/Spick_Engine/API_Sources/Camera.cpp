ffa07002ca72592bbe8db10791c2c5f1c9c21aa4
Bas van Pelt <49717976+sgvanpelt@users.noreply.github.com>
2021-11-09 15:33:26 +0100

    scene camera api and facade

---
+++ Spick_Engine/Spick_Engine/API_Sources/Camera.cpp
@@ -0,0 +1,11 @@
+#include "../API_Headers/Camera.hpp"
+
+using namespace spic;
+
+Color Camera::backgroundColor;
+double Camera::aspectWidth;
+double Camera::aspectHeight;
+
+Camera::Camera(const std::string& name) {
+
+}
