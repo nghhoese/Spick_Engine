a4424b5508aae19f4b399f816d23e9a9a032fc02
Nick Avans <nghhoese@avans.nl>
2021-11-09 17:09:05 +0100

    Extended scene

--- Spick_Engine/Spick_Engine/API_Headers/Scene.hpp
+++ Spick_Engine/Spick_Engine/API_Headers/Scene.hpp
@@ -96,6 +96,7 @@ namespace spic {
     private:
         std::string name;
         std::vector<std::shared_ptr<GameObject>> gameObjects;
+        std::vector<std::shared_ptr<Camera>> cameras;
     };
 
 }
--- Spick_Engine/Spick_Engine/API_Sources/Scene.cpp
+++ Spick_Engine/Spick_Engine/API_Sources/Scene.cpp
@@ -6,19 +6,26 @@ using namespace spic;
 
 SPIC_API Scene::Scene(const std::string& name) {
     gameObjects = std::vector<std::shared_ptr<GameObject>>{};
+    cameras = std::vector<std::shared_ptr<Camera>>{};
 
 }
 
 void Scene::Update() {
-
+    for (std::shared_ptr<GameObject> x : gameObjects) {
+        x->Update();
+    }
 }
 
 void Scene::Render() {
 
+    for (std::shared_ptr<GameObject> x : gameObjects) {
+        x->Render();
+    }
+
 }
 
 void Scene::AddCamera(const Camera& camera) {
-
+   
 }
 
 void Scene::SetActiveCamera(const Camera& camera) {
@@ -38,20 +45,34 @@ Camera& Scene::GetCameraByName(const std::string& cameraName) const {
 }
 
 SPIC_API std::vector<std::shared_ptr<GameObject>> Scene::GetGameObjectsByName(const std::string& gameObjectName) {
-
+    std::vector<std::shared_ptr<GameObject>> objects = std::vector<std::shared_ptr<GameObject>>{};
+    for (std::shared_ptr<GameObject> x : gameObjects) {
+        if (gameObjectName == x->GetName()) {
+            objects.push_back(x);
+       }
+    }
+    return objects;
 }
 
 SPIC_API std::vector<std::shared_ptr<GameObject>> Scene::GetGameObjectsByTag(const std::string& gameObjectTag) {
+    std::vector<std::shared_ptr<GameObject>> objects = std::vector<std::shared_ptr<GameObject>>{};
+    for (std::shared_ptr<GameObject> x : gameObjects) {
+        for (std::string tag : x->GetTags()) {
+            if (gameObjectTag == tag) {
+                objects.push_back(x);
+            }
+        }
 
+    }
+    return objects;
 }
 
 SPIC_API const std::vector<std::shared_ptr<GameObject>> Scene::GetGameObjects() const {
-
+    return gameObjects;
 }
 
 template<class T>
-SPIC_API std::vector<std::shared_ptr<T>> Scene::GetGameObjectsByType() const
-{
+SPIC_API std::vector<std::shared_ptr<T>> Scene::GetGameObjectsByType() const{
     // ... implementation here
 }
 
