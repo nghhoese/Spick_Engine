d8c84de12afc1aa3e3902f334a449cfe474bfeb6
Joery van Egmond <joeryvanegmond@hotmail.com>
2021-11-17 10:31:52 +0100

    init test project

---
+++ Spick_Engine/Spick_Tests/Input.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "API_Headers/Importation.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SpickTests
+{
+	TEST_CLASS(InputTests)
+	{
+	public:
+
+		TEST_METHOD(Check_If_Whether_The_Given_Mouse_Button_Is_Held_Down)
+		{
+			auto anyKey = 0;
+			// Randomise a key from a enum
+			spic::Importation inputManager;
+
+			Assert::
+			Assert::AreEqual(anyKey, 0);
+
+		}
+	};
+}
