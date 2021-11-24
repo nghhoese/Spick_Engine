eccf5ed75649b09e5be60b44af3fe60bf7374c52
Jeroen van der Poel <49712178+Jeroen-van-der-Poel@users.noreply.github.com>
2021-11-24 14:52:43 +0100

    iets

--- Spick_Engine/Spick_Engine/API_Sources/Sprite.cpp
+++ Spick_Engine/Spick_Engine/API_Sources/Sprite.cpp
@@ -20,30 +20,35 @@ void spic::Sprite::OnStart()
 void spic::Sprite::OnUpdate()
 {
 	transform = *GetGameObject()->getTransform();
-	if (player) {
-		imageFacade->setPosition(transform.position.x - GetGameObject()->getScene()->GetActiveCamera()->getX(), transform.position.y - GetGameObject()->getScene()->GetActiveCamera()->getY());
+	if (GetGameObject()->getScene()->GetActiveCamera() != nullptr) {
+		if (player) {
+			imageFacade->setPosition(transform.position.x - GetGameObject()->getScene()->GetActiveCamera()->getX(), transform.position.y - GetGameObject()->getScene()->GetActiveCamera()->getY());
+		}
+		else {
+			int x, y, w, h;
+			x = GetGameObject()->getScene()->GetActiveCamera()->getX();
+			y = GetGameObject()->getScene()->GetActiveCamera()->getY();
+			w = GetGameObject()->getScene()->GetActiveCamera()->getAspectWidth();
+			h = GetGameObject()->getScene()->GetActiveCamera()->getAspectHeight();
+
+			if (x < 0) {
+				GetGameObject()->getScene()->GetActiveCamera()->setX(0);
+			}
+			if (y < 0) {
+				GetGameObject()->getScene()->GetActiveCamera()->setY(0);
+			}
+			if (x > w) {
+				GetGameObject()->getScene()->GetActiveCamera()->setX(w);
+			}
+			if (y > h) {
+				GetGameObject()->getScene()->GetActiveCamera()->setY(h);
+			}
+
+			imageFacade->setPosition(transform.position.x - GetGameObject()->getScene()->GetActiveCamera()->getX(), transform.position.y - GetGameObject()->getScene()->GetActiveCamera()->getY());
+		}
 	}
 	else {
-		int x, y, w, h;
-		x = GetGameObject()->getScene()->GetActiveCamera()->getX();
-		y = GetGameObject()->getScene()->GetActiveCamera()->getY();
-		w = GetGameObject()->getScene()->GetActiveCamera()->getAspectWidth();
-		h = GetGameObject()->getScene()->GetActiveCamera()->getAspectHeight();
-
-		if (x < 0) {
-			GetGameObject()->getScene()->GetActiveCamera()->setX(0);
-		}
-		if (y < 0) {
-			GetGameObject()->getScene()->GetActiveCamera()->setY(0);
-		}
-		if (x > w) {
-			GetGameObject()->getScene()->GetActiveCamera()->setX(w);
-		}
-		if (y > h) {
-			GetGameObject()->getScene()->GetActiveCamera()->setY(h);
-		}
-
-		imageFacade->setPosition(transform.position.x - GetGameObject()->getScene()->GetActiveCamera()->getX(), transform.position.y - GetGameObject()->getScene()->GetActiveCamera()->getY());
+		imageFacade->setPosition(transform.position.x, transform.position.y);
 	}
 	imageFacade->setRotation(transform.rotation);
 	imageFacade->setScale(transform.scale);
