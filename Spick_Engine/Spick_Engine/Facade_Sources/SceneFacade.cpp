cb2b399c18629844cec36274ec459b0187ff203f
Nick Avans <nghhoese@avans.nl>
2021-12-08 15:35:38 +0100

    Changed volume

--- Spick_Engine/Spick_Engine/Facade_Sources/SceneFacade.cpp
+++ Spick_Engine/Spick_Engine/Facade_Sources/SceneFacade.cpp
@@ -45,7 +45,7 @@ int WindowFacade::create_window(const std::string& title, float height, float wi
 			throw Exceptions::SDLInitFailed();
 		}
 		Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT,2,2048);
-
+		Mix_VolumeMusic(50);
 		
 		//Initialize SDL_ttf
 		if (TTF_Init() == -1)
