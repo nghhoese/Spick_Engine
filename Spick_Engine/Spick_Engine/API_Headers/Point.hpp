fd94637c8b69108a404f96a8509bf58a6277b6e9
Joery van Egmond <joeryvanegmond@hotmail.com>
2021-12-08 15:38:02 +0100

    troep

--- Spick_Engine/Spick_Engine/API_Headers/Point.hpp
+++ Spick_Engine/Spick_Engine/API_Headers/Point.hpp
@@ -2,6 +2,8 @@
 #define POINT_H_
 
 #include "SpicHeader.hpp"
+#include <math.h>
+
 
 namespace spic {
 
@@ -9,10 +11,54 @@ namespace spic {
      * @brief Struct representing both a 2D point and a 2D vector.
      */
     struct SPIC_API Point {
+    public:
+
         double x;
         double y;
-    };
 
+        void Sub(Point num) { x -= num.x; y -= num.y; };
+        void Sub(int num) { x -= num; y -= num; };
+        void Sub(Point point1, Point point2) {
+            x = (point1.x - point2.x);
+            y = (point1.y - point2.y);
+        };
+
+        void Mult(Point scalair) { x *= scalair.x; y *= scalair.y; };
+        void Mult(int scalair) { x *= scalair; y *= scalair; };
+
+        void Add(Point num) { x += num.x; y += num.y; };
+        void Add(int num) { x += num; y += num; };
+
+        double Mag() {
+            return sqrt(x * x + y * y);
+        };
+
+        double Dot(Point p) {
+            return (p.x * x + p.y + y);
+        }
+
+        void SetMag(double newMag) {
+            double magnitude = Mag();
+            x = x * newMag / magnitude;
+            y = y * newMag / magnitude;
+        }
+
+        void Limit(double max) {
+            auto normalized = Normalize();
+            auto magnitude = Mag();
+            normalized.Mult((magnitude - max));
+            x = normalized.x;
+            y = normalized.y;
+        }
+
+        Point Normalize() {
+            double magnitude = Mag();
+            Point normalized;
+            normalized.x = x / magnitude;
+            normalized.y = y / magnitude;
+            return normalized;
+        }
+    };
 }
 
 #endif // POINT_H_
