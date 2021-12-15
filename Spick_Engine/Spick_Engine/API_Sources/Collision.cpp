dc2f6ca07ef5cc227921091d38e013e3be7b716e
Joery van Egmond <joeryvanegmond@hotmail.com>
2021-12-15 21:11:49 +0100

    performance fix

--- Spick_Engine/Spick_Engine/API_Sources/Collision.cpp
+++ Spick_Engine/Spick_Engine/API_Sources/Collision.cpp
@@ -12,7 +12,7 @@ std::shared_ptr<spic::BoxCollider> Collision::AABB(spic::GameObject* obj1, std::
         int obj2ColliderWidth = 1;
         int obj2ColliderHeight = 1;
         auto tranform = obj1->getTransform();
-        for (std::shared_ptr<spic::GameObject> obj2 : obj1->getScene().get()->GetGameObjectsByTag(tag)) {
+        for (std::shared_ptr<spic::GameObject> obj2 : obj1->getScene().get()->GetGameObjectsByName(tag)) {
                         std::shared_ptr<spic::BoxCollider> collider2 = std::dynamic_pointer_cast<spic::BoxCollider>(obj2->GetComponent <spic::BoxCollider>());
                         if (collider2 != nullptr) {
                             obj2ColliderWidth = collider2->Width();
